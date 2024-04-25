bd391d63d8715ca80577cba6c324654d9730f263
Yadev Jayachandran <yadev64@gmail.com>
2024-04-26 03:16:46 +0530

    layout fixes

--- Editor/src/Layouts/MainLayout.tsx
+++ Editor/src/Layouts/MainLayout.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import TopNavBar from "../Components/TopNavBar";
 
 function MainLayout() {
   return (
-    <div>
+      <div>
+          <TopNavBar></TopNavBar>
       <div>Hello Main Layout</div>
+      {/* PS: The Outlet is where the children defined in the AppRoute file will be rendered.  */}
       <Outlet />
     </div>
   );
